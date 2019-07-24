4e50ad8aabda301cfb1ec9299a5f36fbda99940b
Nivaaz <nksehmbhi@gmail.com>
2019-07-24 10:59:31 +1000

    update 24/7

--- lab4.m
+++ lab4.m
@@ -1,21 +1,22 @@
+    
 %% Question 1
 s = tf('s');
 
-kdc = 46.9/2;
-tau = 132*10^-3;
+Kdc_Iden = 46.9/2;
+tau_Iden = 132*10^-3;
 ts = 0.35;
 os = 0.03;
 
 zeta = -log(os)/(sqrt(pi^2 + log(os)^2))
 wn = 4/(zeta*ts)
 
-kp = (2*zeta*wn*tau-1)/kdc
-ki = wn^2*tau/kdc
+Kp = (2*zeta*wn*tau_Iden-1)/Kdc_Iden
+Ki = wn^2*tau_Iden/Kdc_Iden
 
-zeta1 = (kp*kdc+1)/(2*sqrt(ki*kdc*tau))
-wn1 = sqrt(kdc*ki/tau)
-K = kdc*kp/tau
-a = ki/kp
+zeta1 = (Kp*Kdc_Iden+1)/(2*sqrt(Ki*Kdc_Iden*tau_Iden))
+wn1 = sqrt(Kdc_Iden*Ki/tau_Iden)
+K = Kdc_Iden*Kp/tau_Iden
+a = Ki/Kp
 tf1 = K*(s+a)/(s^2 + 2*wn1*s*zeta1 + wn1^2)
 
 
@@ -26,5 +27,3 @@ step(tf1*50);
 
 
 %%
-
-
